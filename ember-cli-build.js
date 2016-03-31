4246c158211ec95f3c706814ab57a3989f1155bd
Jesse Pinho <jesse@jessepinho.com>
2016-03-31 14:37:17 -0700

    Add MDL to the project

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,19 @@
 /* global require, module */
 
-var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
+var Angular2App = require('angular-cli/lib/broccoli/angular2-app'),
+    BroccoliSass = require('broccoli-sass'),
+    mergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
   var app = new Angular2App(defaults, {
-    vendorNpmFiles: []
-  });
-  return app.toTree();
+        vendorNpmFiles: [
+          'material-design-lite/material.js'
+        ]
+      }),
+      cssTree = new BroccoliSass(['src/app/styles', 'node_modules/material-design-lite/src'], './app.sass', 'styles.css');
+
+  return mergeTrees([
+    app.toTree(),
+    cssTree
+  ]);
 };
